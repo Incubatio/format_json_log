c9a262c16d8e56b8dc120c89515970c9e6a71b13
incubatio <work@incubatio.org>
2019-12-20 17:17:06 -0500

    add support for valid raw json data, unsupported level of log are no-longer parsed, empty stacktrace are no longer displayed

--- pkg/formatlog.go
+++ pkg/formatlog.go
@@ -10,11 +10,20 @@ import (
 )
 
 type _JSONLog struct {
-	Logger     string
-	Level      string
-	Msg        string
-	Error      string
-	Stacktrace string
+	//Logger     string
+	Level      string `json:",omitempty"`
+	Msg        string `json:",omitempty"`
+	Error      string `json:",omitempty"`
+	Stacktrace string `json:",omitempty"`
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
@@ -24,11 +33,14 @@ func main() {
 	const WARNING = "warning"
 	const INFO = "info"
 	const TITLE = "title"
+	const JSON = "json"
 	colors := map[string]*color.Color{}
 	colors[ERROR] = color.New(color.FgRed, color.Bold)
 	colors[WARNING] = color.New(color.FgYellow, color.Bold)
 	colors[INFO] = color.New(color.FgGreen, color.Bold)
+	colors[JSON] = color.New(color.FgMagenta, color.Bold)
 	colors[TITLE] = color.New(color.FgWhite).Add(color.Underline)
+	levels := []string{ERROR, WARNING, INFO}
 
 	colorize := func(str string, level string) string {
 		if level != "" {
@@ -53,35 +65,54 @@ func main() {
 	var StrackTraceLabel = colorize("Stacktrace:", TITLE)
 
 	var parsedLog *_JSONLog
+	var result map[string]interface{}
+	var data []byte
 	for {
 		inputText, _ = reader.ReadString('\n')
 		inputLen = len(inputText) - 1
 		output = ""
 
 		if inputLen > 0 {
-			parseError = json.Unmarshal([]byte(inputText), &parsedLog)
-			if parseError != nil || parsedLog == nil || parsedLog.Level == "" {
+			parseError = json.Unmarshal([]byte(inputText), &result)
+			if parseError != nil || result == nil {
 				addLineBreak = prevLevel != "" && prevLevel != ERROR
 				prevLevel = ""
 				//output = "/!\\ Failed to parse Input /!\\"
 				output = "!> " + strings.Replace(inputText, "\n", "", -1)
 			} else {
-				addLineBreak = prevLevel != parsedLog.Level && prevLevel != ERROR
-				prevLevel = parsedLog.Level
-				output += "[" + colorize(strings.ToUpper(parsedLog.Level), parsedLog.Level) + "] " + parsedLog.Msg
-				//fmt.Println("->[DEBUG] " + inputText)
-				if parsedLog.Error != "" || parsedLog.Stacktrace != "" {
-					stacktrace = strings.Replace(parsedLog.Stacktrace, "\t", "  ", -1)
-					stacktrace = strings.Replace("\n"+stacktrace, "\n", "\n    ", -1)
-					output += " \n  " + descriptionLabel + " " + parsedLog.Error + "\n  " + StrackTraceLabel + " " + stacktrace + "\n"
+				parseError = json.Unmarshal([]byte(inputText), &parsedLog)
+				if parsedLog.Level == "" || !stringInSlice(parsedLog.Level, levels) {
+					//output = "!> " + strings.Replace(inputText, "\n", "", -1)
+					//parseError = json.UnmarshalIndent([]byte(inputText), &parsedLog)
+					data, parseError = json.MarshalIndent(result, "", "  ")
+					if parseError != nil {
+						output = "!> " + strings.Replace(inputText, "\n", "", -1)
+					} else {
+						prevLevel = "json"
+						output = "[" + colorize(prevLevel, prevLevel) + "]\n" + string(data)
+					}
+				} else {
+					addLineBreak = prevLevel != parsedLog.Level && prevLevel != ERROR
+					prevLevel = parsedLog.Level
+					output += "[" + colorize(strings.ToUpper(parsedLog.Level), parsedLog.Level) + "] " + parsedLog.Msg
+					//fmt.Println("->[DEBUG] " + inputText)
+					if parsedLog.Error != "" {
+						output += " \n  " + descriptionLabel + " " + parsedLog.Error
+					}
+					if parsedLog.Stacktrace != "" {
+						stacktrace = strings.Replace(parsedLog.Stacktrace, "\t", "  ", -1)
+						stacktrace = strings.Replace("\n"+stacktrace, "\n", "\n    ", -1)
+						output += "\n  " + StrackTraceLabel + " " + stacktrace + "\n"
+					}
+					//fmt.Printf("->[DEBUG]%+v\n", parsedLog)
 				}
-				//fmt.Printf("->[DEBUG]%+v\n", parsedLog)
 			}
 			if addLineBreak {
 				output = "\n" + output
 			}
 			fmt.Println(output)
 			parsedLog = nil
+			result = nil
 		}
 
 	}
